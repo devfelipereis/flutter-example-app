d9bf8afbcb8ae42155f29fb8cc78e2529be02d08
Felipe <feelipereis7@gmail.com>
2021-03-03 18:15:37 -0300

    web support

--- lib/core/dio/jsonplaceholder_dio.dart
+++ lib/core/dio/jsonplaceholder_dio.dart
@@ -1,32 +1,30 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http_certificate_pinning/certificate_pinning_interceptor.dart';
 
 class JsonPlaceholderDioClient {
-  late Dio _dio;
+  final Dio _dio = Dio(BaseOptions(
+    connectTimeout: 5000,
+    receiveTimeout: 5000,
+    sendTimeout: 5000,
+    baseUrl: env['JSON_PLACEHOLDER_API_URL'],
+  ));
 
   JsonPlaceholderDioClient() {
-    _dio = Dio(baseOptions());
     addInterceptors();
   }
 
   Dio get instance => _dio;
 
-  BaseOptions baseOptions() {
-    return BaseOptions(
-      connectTimeout: 5000,
-      receiveTimeout: 5000,
-      sendTimeout: 5000,
-      baseUrl: env['JSON_PLACEHOLDER_API_URL'],
-    );
-  }
-
   void addInterceptors() {
     // _dio.interceptors.add(LogInterceptor());
     // _dio.interceptors.add(tokenInterceptor());
     ///
-    _dio.interceptors.add(CertificatePinningInterceptor([
-      '27:9B:88:6D:65:D6:F1:09:46:7F:E2:21:70:DA:AC:32:69:B8:0E:CB:DB:F6:77:AA:2E:68:3D:1F:68:A8:DF:80'
-    ]));
+    if (!kIsWeb) {
+      _dio.interceptors.add(CertificatePinningInterceptor([
+        '27:9B:88:6D:65:D6:F1:09:46:7F:E2:21:70:DA:AC:32:69:B8:0E:CB:DB:F6:77:AA:2E:68:3D:1F:68:A8:DF:80'
+      ]));
+    }
   }
 }
